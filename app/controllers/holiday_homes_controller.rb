20fa6a17a0e374938cda3e68ac15c61ebf4df6ce
Bluette1 <marylene.sawyer@gmail.com>
2021-01-31 10:28:50 +0200

    Correct the syntax if else post create method

--- app/controllers/holiday_homes_controller.rb
+++ app/controllers/holiday_homes_controller.rb
@@ -16,13 +16,13 @@ class HolidayHomesController < ApplicationController
   def create
     @holiday_home = current_user.holiday_homes.build(holiday_home_params)
     
-    if (@holiday_home.save) {
+    if @holiday_home.save
       render json: @holiday_home
-    } else {
+    else 
       p @holiday_home.errors.full_messages
 
       result json: @holiday_home.errors.full_messages
-    }
+    end
   end
 
   # DELETE /holiday_homes:id
